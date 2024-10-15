0c45bc37b428e3a58775d51afce2c6bc5de328e5
Mayck Lima <mayckclima@gmail.com>
2024-10-15 00:48:01 -0300

    Ajustando a plicaçao para subir

--- frontend/src/app/pages/movimentacao/movimentacao-add-edit-dialog/movimentacao-add-edit-dialog.component.ts
+++ frontend/src/app/pages/movimentacao/movimentacao-add-edit-dialog/movimentacao-add-edit-dialog.component.ts
@@ -1,13 +1,13 @@
-import { Component, EventEmitter, Inject, Output } from '@angular/core';
-import { MaterialModule } from '../../../shared/material.module';
 import { CommonModule } from '@angular/common';
-import { CategoriaService } from '../../categoria/categoria.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { tap } from 'rxjs';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MovimentacoesService } from '../movimentacoes/movimentacoes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { tap } from 'rxjs';
+import { MaterialModule } from '../../../shared/material.module';
+import { CategoriaService } from '../../categoria/categoria.service';
 import { buscarPorChave } from '../model/TipoMovimentacao';
+import { MovimentacoesService } from '../movimentacoes/movimentacoes.service';
 
 @Component({
   selector: 'app-movimentacao-add-edit-dialog',
@@ -19,19 +19,19 @@ import { buscarPorChave } from '../model/TipoMovimentacao';
 
 
 export class MovimentacaoAddEditDialogComponent {
-  
+
   formulario!: FormGroup;
   categorias: any;
 
   constructor(
-    private _formBuilder: FormBuilder, 
-    private _snackBar: MatSnackBar,
-    private _categoriaService: CategoriaService,
-    private _dialogRef: MatDialogRef<MovimentacaoAddEditDialogComponent>,
-    private _movimentacoesService: MovimentacoesService,
-    @Inject(MAT_DIALOG_DATA) public data: any){}
-  
-    ngOnInit() {
+    private readonly _formBuilder: FormBuilder,
+    private readonly _snackBar: MatSnackBar,
+    private readonly _categoriaService: CategoriaService,
+    private readonly _dialogRef: MatDialogRef<MovimentacaoAddEditDialogComponent>,
+    private readonly _movimentacoesService: MovimentacoesService,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
     this.inicializarFormulario();
     this.getCategorias();
 
@@ -40,69 +40,73 @@ export class MovimentacaoAddEditDialogComponent {
 
     this.formulario.patchValue(this.data);
     console.log(this.formulario)
-    }
+  }
 
-    inicializarFormulario(){
-      this.formulario = this._formBuilder.group({
-        id: [],
-        descricao:[null, Validators.required],
-        valor: [0, Validators.required],
-        tipo: [null, Validators.required],
-        pago: [false, Validators.required],
-        data: [new Date(), Validators.required],
-        dataInclusao: [new Date(), Validators.required],
-        conta:[null],
-        categoria: [null, Validators.required]
-      });
-    }
+  inicializarFormulario() {
+    this.formulario = this._formBuilder.group({
+      id: [],
+      descricao: [null, Validators.required],
+      valor: [0, Validators.required],
+      tipo: [null, Validators.required],
+      pago: [false, Validators.required],
+      data: [new Date(), Validators.required],
+      dataInclusao: [new Date(), Validators.required],
+      conta: [null],
+      categoria: [null, Validators.required]
+    });
+  }
 
-    getCategorias(): void {
-      this._categoriaService.getCategorias()
-        .pipe(
-          tap({
-            next: (data) => {
-              this.categorias = data;
-              console.log('this.categorias')// Adiciona os dados recebidos à variável dataToDisplay
-              console.log(this.categorias)// Adiciona os dados recebidos à variável dataToDisplay
-            },
-            error: (error) => {
-              console.error('Erro ao obter as movimentações:', error);
-            }
-          })
-        )
-        .subscribe();
-    }
+  getCategorias(): void {
+    this._categoriaService.getCategorias()
+      .pipe(
+        tap({
+          next: (data) => {
+            this.categorias = data;
+            console.log('this.categorias')// Adiciona os dados recebidos à variável dataToDisplay
+            console.log(this.categorias)// Adiciona os dados recebidos à variável dataToDisplay
+          },
+          error: (error) => {
+            console.error('Erro ao obter as movimentações:', error);
+          }
+        })
+      )
+      .subscribe();
+  }
 
-    compareFunction(o1: any, o2: any) {
-      return o1.name == o2.name && o1.id == o2.id;
-    }
+  compareFunction(o1: any, o2: any) {
+    return o1.name == o2.name && o1.id == o2.id;
+  }
 
-    salvar(){
-      console.log(this.formulario)
-      this._movimentacoesService.adicionarMovimentacao(this.formulario.value).subscribe(resultado => {
-        console.log(resultado)
-        if(resultado){
-          this._snackBar.open("Movimentação salva", "Fechar", {
-            duration: 2000,
-            verticalPosition: 'bottom',
-            panelClass: 'notify-successful'
+  salvar() {
+    console.log(this.formulario)
+    this._movimentacoesService.adicionarMovimentacao(this.formulario.value).subscribe(resultado => {
+      console.log(resultado)
+      if (resultado) {
+        this._snackBar.open("Movimentação salva", "Fechar", {
+          duration: 2000,
+          verticalPosition: 'bottom',
+          panelClass: 'notify-successful'
         });
-          this._dialogRef.close();
-        }
-      });
-    }
+        this._dialogRef.close();
+      }
+    });
+  }
 
-    cancelar(){
-      this._dialogRef.close();
-    }
+  cancelar() {
+    this._dialogRef.close();
+  }
 
-    getTipoMovimentacao(){
-      return buscarPorChave(this.formulario.get('tipo')?.value);
-    }
+  getTipoMovimentacao() {
+    return buscarPorChave(this.formulario.get('tipo')?.value);
+  }
+
+  alterarPago(value: boolean) {
+
+  }
 }
 
 
-  
+
 
 
 
