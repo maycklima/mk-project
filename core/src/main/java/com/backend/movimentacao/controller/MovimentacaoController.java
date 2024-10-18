4073d14ac26e1c87e748998cea2e757f413c39c4
Mayck Lima <mayckclima@gmail.com>
2024-10-18 01:09:18 -0300

    ajustes

--- core/src/main/java/com/backend/movimentacao/controller/MovimentacaoController.java
+++ core/src/main/java/com/backend/movimentacao/controller/MovimentacaoController.java
@@ -5,6 +5,7 @@ import com.backend.movimentacao.model.MovimentacaoFilter;
 import com.backend.movimentacao.service.MovimentacaoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/movimentacao")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MovimentacaoController {
 
     @Autowired
--- core/src/main/resources/import.sql
+++ core/src/main/resources/import.sql
@@ -9,7 +9,7 @@ insert into tb_categoria (descricao) values ('Minha conta');
 insert into tb_categoria (descricao) values ('Inter');
 insert into tb_categoria (descricao) values ('Gastos Débitos');
 
---FIXO
+--OUTUBRO
 insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Salário',   10000, FALSE, 7, '2024-10-01', 'ENTRADA', '2024-02-27');
 
 insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Energia',   -195.90, FALSE, 1, '2024-10-01', 'SAIDA', '2024-02-27');
@@ -55,3 +55,96 @@ insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, d
 insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Inter 2', -96.34,  FALSE, 8, '2024-10-01', 'SAIDA', '2024-02-12');
 
 insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Débito',  -1685.10,  FALSE, 9, '2024-10-01', 'SAIDA', '2024-02-12');
+
+--NOVEMBRO
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Salário',   7000, FALSE, 7, '2024-11-01', 'ENTRADA', '2024-02-27');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Energia',   -195.90, FALSE, 1, '2024-11-01', 'SAIDA', '2024-02-27');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Água',      -88.19,  FALSE, 1, '2024-11-01', 'SAIDA', '2024-02-26');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Carro',     -638.02, FALSE, 1, '2024-11-01', 'SAIDA', '2024-02-25');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Internete', -359.80, FALSE, 1, '2024-11-01', 'SAIDA', '2024-02-25');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Faculdade', -262.51, FALSE, 1, '2024-11-01', 'SAIDA', '2024-02-25');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 1', -79.73,  FALSE, 2, '2024-11-01', 'SAIDA', '2024-02-24');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 2', -51.86,  FALSE, 2, '2024-11-01', 'SAIDA', '2024-02-23');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 8', -118,    FALSE, 2, '2024-11-01', 'SAIDA', '2024-02-17');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 9', -100,    FALSE, 2, '2024-11-01', 'SAIDA', '2024-02-16');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 1', -135.98, FALSE, 3, '2024-11-01', 'SAIDA', '2024-02-16');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 3', -333.25, FALSE, 3, '2024-11-01', 'SAIDA', '2024-02-14');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 8', -161.53, FALSE, 3, '2024-11-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 9', -80,     FALSE, 3, '2024-11-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Casas Bahias 1', -120.79, FALSE, 6, '2024-11-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Casas Bahias 2', -141.99, FALSE, 6, '2024-11-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard Uniclass 1', -499.99, FALSE, 4, '2024-11-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 1', -14.90,  FALSE, 5, '2024-11-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 2', -14.90,  FALSE, 5, '2024-11-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 6', -135.04, FALSE, 5, '2024-11-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 7', -79.80,  FALSE, 5, '2024-11-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Inter 2', -96.34,  FALSE, 8, '2024-11-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Débito',  -600.10,  FALSE, 9, '2024-11-01', 'SAIDA', '2024-02-12');
+
+--DEZEMBRO
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Salário',   8000, FALSE, 7, '2024-12-01', 'ENTRADA', '2024-02-27');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Energia',   -195.90, FALSE, 1, '2024-12-01', 'SAIDA', '2024-02-27');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Água',      -88.19,  FALSE, 1, '2024-12-01', 'SAIDA', '2024-02-26');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Carro',     -638.02, FALSE, 1, '2024-12-01', 'SAIDA', '2024-02-25');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Internete', -359.80, FALSE, 1, '2024-12-01', 'SAIDA', '2024-02-25');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Faculdade', -262.51, FALSE, 1, '2024-12-01', 'SAIDA', '2024-02-25');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 1', -79.73,  FALSE, 2, '2024-12-01', 'SAIDA', '2024-02-24');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 2', -51.86,  FALSE, 2, '2024-12-01', 'SAIDA', '2024-02-23');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 1', -135.98, FALSE, 3, '2024-12-01', 'SAIDA', '2024-02-16');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 6', -285.35, FALSE, 3, '2024-12-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 7', -100,    FALSE, 3, '2024-12-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 8', -161.53, FALSE, 3, '2024-12-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 9', -80,     FALSE, 3, '2024-12-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Casas Bahias 1', -120.79, FALSE, 6, '2024-12-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Casas Bahias 2', -141.99, FALSE, 6, '2024-12-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard Uniclass 1', -499.99, FALSE, 4, '2024-12-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 1', -14.90,  FALSE, 5, '2024-12-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 2', -14.90,  FALSE, 5, '2024-12-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Inter 1', -284.25, FALSE, 8, '2024-12-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Inter 2', -96.34,  FALSE, 8, '2024-12-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Débito',  -500.10,  FALSE, 9, '2024-12-01', 'SAIDA', '2024-02-12');
+
+--JANEIRO
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Salário',   5000, FALSE, 7, '2025-01-01', 'ENTRADA', '2024-02-27');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Energia',   -195.90, FALSE, 1, '2025-01-01', 'SAIDA', '2024-02-27');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Água',      -88.19,  FALSE, 1, '2025-01-01', 'SAIDA', '2024-02-26');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Carro',     -638.02, FALSE, 1, '2025-01-01', 'SAIDA', '2024-02-25');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Internete', -359.80, FALSE, 1, '2025-01-01', 'SAIDA', '2024-02-25');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Faculdade', -262.51, FALSE, 1, '2025-01-01', 'SAIDA', '2024-02-25');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 1', -79.73,  FALSE, 2, '2025-01-01', 'SAIDA', '2024-02-24');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Nubank 2', -51.86,  FALSE, 2, '2025-01-01', 'SAIDA', '2024-02-23');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 7', -100,    FALSE, 3, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 8', -161.53, FALSE, 3, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard 9', -80,     FALSE, 3, '2025-01-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Itaucard Uniclass 1', -499.99, FALSE, 4, '2025-01-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 1', -14.90,  FALSE, 5, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 2', -14.90,  FALSE, 5, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 3', -59.89,  FALSE, 5, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 4', -54.29,  FALSE, 5, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 5', -38.99,  FALSE, 5, '2025-01-01', 'SAIDA', '2024-02-12');
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Will 7', -79.80,  FALSE, 5, '2025-01-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Inter 2', -96.34,  FALSE, 8, '2025-01-01', 'SAIDA', '2024-02-12');
+
+insert into tb_movimentacao (descricao, valor, pago, categoria_id, data, tipo, data_inclusao) values ('Gasto Débito',  -200.10,  FALSE, 9, '2025-01-01', 'SAIDA', '2024-02-12');
